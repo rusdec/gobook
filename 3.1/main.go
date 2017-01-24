d4c8a758546b1763bd2fa71bc09e3c7dda523371
Алексей Мехоношин <rusdec@MacBook-Pro-Aleksej-2.local>
2017-01-24 22:58:58 +0300

    3.1 готово

--- 3.1/main.go
+++ 3.1/main.go
@@ -12,6 +12,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"errors"
 )
 
 const (
@@ -29,10 +30,22 @@ func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' style='stroke: grey; fill: white; stroke-width: 0.7' width='%d' height='%d'>\n", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
+			ax, ay, err := corner(i+1, j)
+			if err != nil {
+				continue
+			}
+			bx, by, err := corner(i, j)
+			if err != nil {
+				continue
+			}
+			cx, cy, err := corner(i, j+1)
+			if err != nil {
+				continue
+			}
+			dx, dy, err := corner(i+1, j+1)
+			if err != nil {
+				continue
+			}
 			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
 		}
 		
@@ -40,15 +53,19 @@ func main() {
 	fmt.Printf("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, error) {
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 	z := f(x,y)
 	
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	
-	return sx, sy	
+
+	if math.IsNaN(sx) || math.IsNaN(sy) {
+		return sx, sy, errors.New("nan")
+	}
+	 	
+	return sx, sy, nil	
 }
 
 func f(x, y float64) float64 {
