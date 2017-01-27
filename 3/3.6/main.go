58ca4cc15295f4e32dc4568d607b317460c0df1d
Алексей Мехоношин <rusdec@MacBook-Pro-Aleksej-2.local>
2017-01-27 22:13:03 +0300

    3.6 готово

--- 3/3.6/main.go
+++ 3/3.6/main.go
@@ -25,11 +25,12 @@ func main() {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	
 	for py := 0; py <= height; py++ {
-		y := float64(py) / height * (ymax - ymin) + ymin
+		y	:= float64(py) / height * (ymax - ymin) + ymin
+		y1	:= float64(py+1) / height * (ymax - ymin) + ymin
 		for px := 0; px <= width; px++ {
-			x := float64(px) / width * (xmax - xmin) + xmin
-			z := complex(x, y)
-			img.Set(px, py, mandelbrot(z))
+			x	:= float64(px) / width * (xmax - xmin) + xmin
+			x1	:= float64(px+1) / width * (xmax - xmin) + xmin
+			img.Set(px, py, superSampling(x, y, x1, y1))
 			
 		}
 	}
@@ -37,21 +38,46 @@ func main() {
 	png.Encode(os.Stdout, img)
 }
 
-func mandelbrot(z complex128) color.Color {
+func superSampling(x, y, x1, y1 float64) color.Color{
+	colors := [3]uint16{}
+	z := complex(x, y)
+	for i, k := range mandelbrot(z) {
+		colors[i] += k
+	}
+	z = complex(x1, y)
+	for i, k := range mandelbrot(z) {
+		colors[i] += k
+	}
+	z = complex(x, y1)
+	for i, k := range mandelbrot(z) {
+		colors[i] += k
+	}
+	z = complex(x1, y1)
+	for i, k := range mandelbrot(z) {
+		colors[i] += k
+	}
+	for i, k := range colors {
+		colors[i] = k/4
+	}
+	
+	return color.YCbCr{uint8(colors[0]), uint8(colors[1]), uint8(colors[2])}
+}
+
+func mandelbrot(z complex128) ([3]uint16) {
 	const (
 		iterations = 200
 		contrast = 15
 	)
 	
 	var v complex128
-	var colorMax uint8 = 128
+	var colorMax uint16 = 128
 
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.YCbCr{16, colorMax, colorMax} //цвет точки фона
+			return [3]uint16{16, colorMax, colorMax}
 		}
 	}
 	
-	return color.YCbCr{161,118,10} //цвет точки фрактала
+	return [3]uint16{161,118,10}
 }
